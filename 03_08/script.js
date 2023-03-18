9ff53ed96385622394c164cd398f2f75af878463
Elianis Medina <elianismedina05@outlook.com>
2023-03-18 18:01:12 -0500

    changes to files unde js-course
    changes to files unde js-course

--- 03_08/script.js
+++ 03_08/script.js
@@ -11,10 +11,12 @@ const backpack = {
     left: 26,
     right: 26,
   },
+  // Este es un método porque crea una funcion dentro de un objeto, esta funcion tiene 1 parametro llamado lidStatus. El nombre del método es toggleLid
   lidOpen: false,
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
+  // Este es un método porque crea una funcion dentro de un objeto
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
@@ -23,3 +25,6 @@ const backpack = {
 
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
+console.log("Left before:", backpack.strapLength.left);
+backpack.newStrapLength(10, 15);
+console.log("Left after:", backpack.strapLength.left);
